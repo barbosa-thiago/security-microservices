ac77bccfd0affa46117a4fbbadee1a4af88b9875
barbosa-thiago <cartaparathiago@gmail.com>
2021-08-12 10:59:03 -0300

    fixed service and passwordencoder beans missing

--- valhalla/src/main/java/com/thiago/valhallaproject/config/SecurityConfig.java
+++ valhalla/src/main/java/com/thiago/valhallaproject/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.thiago.valhallaproject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,6 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().httpBasic()
+                .and().formLogin();
+    }
+    @Bean
+    public PasswordEncoder passwordBean(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- valhalla/src/main/java/com/thiago/valhallaproject/controller/GodNameController.java
+++ valhalla/src/main/java/com/thiago/valhallaproject/controller/GodNameController.java
@@ -2,6 +2,7 @@ package com.thiago.valhallaproject.controller;
 
 import com.thiago.valhallaproject.domain.GodName;
 import com.thiago.valhallaproject.service.GodNameService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("names")
+@RequiredArgsConstructor
 public class GodNameController {
 
-    private GodNameService godNameService;
+    private final GodNameService godNameService;
 
     @GetMapping
     public ResponseEntity<Iterable<GodName>> findAll(Pageable pageable) {
