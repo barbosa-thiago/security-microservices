f3e452f485badbb6d5b939dfbc2aba0a95ab066a
barbosa-thiago <cartaparathiago@gmail.com>
2021-08-12 21:39:49 -0300

    gateway service and passwordencoder

--- valhalla/src/main/java/com/thiago/valhallaproject/ValhallaApplication.java
+++ valhalla/src/main/java/com/thiago/valhallaproject/ValhallaApplication.java
@@ -2,10 +2,8 @@ package com.thiago.valhallaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ValhallaApplication {
 
     public static void main(String[] args) {
